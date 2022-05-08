33aff32b5b20467e43465b7eeb73149ed0040327
achiar99 <34912231+achiar99@users.noreply.github.com>
2022-05-08 16:11:52 +0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,19 @@
-
+resource "aws_s3_bucket" "sample_bucket_achia_prisma" {
+  # bucket is not encrypted
+  bucket = "sample-bucket-achia-prisma"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  force_destroy = true
+  tags = {
+    git_commit           = "77e689e3acd6a0e1e79a5f78ddce241085dac820"
+    git_file             = "aws/s3.tf"
+    git_last_modified_at = "2022-01-05 09:41:31"
+    git_last_modified_by = "tron47@gmail.com"
+    git_modifiers        = "tron47"
+    git_org              = "tronxd"
+    git_repo             = "terraform_samples"
+    yor_trace            = "c0d78c48-5f9e-4187-a04e-a6bb0e7ed9f8"
+  }
+}
