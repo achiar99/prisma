e0e45334f94791102c09d4251e84ded02ccd3947
achiar99 <34912231+achiar99@users.noreply.github.com>
2022-05-10 16:11:27 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,8 @@
+resource azurerm_security_center_subscription_pricing "pricing" {
+  tier = "Free"
+}
+resource azurerm_security_center_contact "contact2222" {
+  alert_notifications = false
+  alerts_to_admins    = false
+  email               = "some@email.com"
+}
